922b0f3c2be7ca193f25190f8db0bddb1b86fc3d
vincep93 <156262496+vincep93@users.noreply.github.com>
2024-02-21 17:39:12 -0800

    First Upload

---
+++ Rock-Paper-Scissors/Javascript/script.js
@@ -0,0 +1,76 @@
+//make a value for Rock. Make value greater than Scissors.
+//make a value for Scissors. Make value greater than Paper.
+//make a value for Paper. Make value greater than Rock.
+
+//create a function for the computer to select a value randomly 
+
+//create a function that takes two parameters and returns a srting that declares a winner or tie
+
+//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+//Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end.
+
+
+
+const Rock;
+const Paper;
+const Scissors;
+
+let Rock > Scissors;
+let Paper > Rock;
+let Scissors > Paper;
+
+function computerSelect()
+
+let randomChoice = Math.floor(Math.random()* 3 );
+    if (randomChoice === 2) {
+        return choice[2];
+    } else if (randomChoice === 1) {
+        return choice[1];
+    } else (randomChoice === 0) {
+        return choice [0];
+    }
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+
+// player computer tie here 
+if (playerSelection === Rock && computerSelect === Rock) {
+    return prompt("Its a Tie. Pick again!");
+} else if {
+    (playerSelection === Paper && computerSelect === Paper) {
+        return prompt("Its a Tie. Pick again!");
+} else if {
+    (playerSelection === Scissors && computerSelect === SCissors) {
+            return prompt("Its a Tie. Pick again!");
+    }
+}
+}
+
+
+//player loss
+    if (playerSelection === Rock && computerSelect === Paper) {
+        return prompt("Paper beats Rock. You lose.")
+    } else if {
+        playerSelection = Paper, computerSelect = Scissors {
+            return prompt("Scissors beats Paper. You lose.")
+        }
+    } else if {
+        (playerSelection === Scissors && computerSelect === Scisors) {
+            return prompt("Its a Tie. Pick again!")
+    }
+  }
+  
+
+//player win
+  if (playerSelection === Rock && computerSelect === Scissors) {
+        return prompt ()
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
