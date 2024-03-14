3b0a09bd940a493fd7c28d78ce2ad066d5717667
vincep93 <pellegrinov93@gmail.com>
2024-03-14 13:48:44 -0700

    Completed!

--- Rock-Paper-Scissors/js/script.js
+++ Rock-Paper-Scissors/js/script.js
@@ -1,40 +1,9 @@
-//make a value for Rock. Make value greater than Scissors.
-//make a value for Scissors. Make value greater than Paper.
-//make a value for Paper. Make value greater than Rock.
-
-//create a function for the computer to select a value randomly 
-
-//create a function that takes two parameters and returns a srting that declares a winner or tie
-
-//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-//Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end.
-
-
-
-
-
-
-//Note from Discord - ur function i.e: (Math.floor(Math.random() * 3))) executes and returns a random value which is stored into the randomChoice variable. So, it always uses that value unless u re-assign it 🤔 
-
-//for function computerSelect(randomChoice) I think i need to put in the play function after it and tie it to the player choice to get this to work ie: function computerSelect(randomChoice) {if (computerSelect = 0) return Rock}
-
-
-
-
-
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
 let playerScore = 0;
 let computerScore= 0;
-
-
-//const playerSelection = "rock" || "paper" || "scissors";
-
-
-
 
 function pickRock() {
     playerSelection = "rock";
